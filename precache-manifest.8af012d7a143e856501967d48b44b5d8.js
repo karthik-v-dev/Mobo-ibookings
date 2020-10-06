360d07fa2d70dace868e4e66a3681287da80bcd5
karthik-v-dev <karthikvoorugonda@gmail.com>
2020-10-06 06:04:06 +0530

    Updates

---
+++ precache-manifest.8af012d7a143e856501967d48b44b5d8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d430d765e340d52b25d62d8dec0c35",
+    "url": "/Mobo-ibookings/index.html"
+  },
+  {
+    "revision": "f9f74c4fea31638520e6",
+    "url": "/Mobo-ibookings/static/css/2.828be9a0.chunk.css"
+  },
+  {
+    "revision": "db721cedcf4b9019cac6",
+    "url": "/Mobo-ibookings/static/css/main.bc46e7db.chunk.css"
+  },
+  {
+    "revision": "f9f74c4fea31638520e6",
+    "url": "/Mobo-ibookings/static/js/2.a152b979.chunk.js"
+  },
+  {
+    "revision": "24b8054ddc1445462862104fe5fc876e",
+    "url": "/Mobo-ibookings/static/js/2.a152b979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db721cedcf4b9019cac6",
+    "url": "/Mobo-ibookings/static/js/main.771ea9e6.chunk.js"
+  },
+  {
+    "revision": "e6f2568aa7e53ad54820",
+    "url": "/Mobo-ibookings/static/js/runtime-main.d896c146.js"
+  },
+  {
+    "revision": "79a8ded71b8f676e52916ab1aed17cbd",
+    "url": "/Mobo-ibookings/static/media/Ibapp.79a8ded7.png"
+  },
+  {
+    "revision": "19c7609ec2a7cd82fb1202c87b9f24d0",
+    "url": "/Mobo-ibookings/static/media/img1.19c7609e.jpg"
+  },
+  {
+    "revision": "587c16feff36053675ef7c1a49e4afed",
+    "url": "/Mobo-ibookings/static/media/img10.587c16fe.jpg"
+  },
+  {
+    "revision": "456d23be1551fd5a7f6101a1ae3bba6c",
+    "url": "/Mobo-ibookings/static/media/img3.456d23be.jpg"
+  },
+  {
+    "revision": "17ac45c4395b6f4d136e912408298fed",
+    "url": "/Mobo-ibookings/static/media/img4.17ac45c4.jpg"
+  },
+  {
+    "revision": "322cba6907acbf32370a753ac8c3fe1f",
+    "url": "/Mobo-ibookings/static/media/img5.322cba69.jpg"
+  },
+  {
+    "revision": "3767d803baba697b3a24cbe5215e349a",
+    "url": "/Mobo-ibookings/static/media/img6.3767d803.jpg"
+  },
+  {
+    "revision": "d9bdea92848f5fba6a50a0fe3051a2ac",
+    "url": "/Mobo-ibookings/static/media/websiteimg11.d9bdea92.png"
+  }
+]);
